22272c4c19287f0392afcdc4499ccfff448263c3
Stan <stitrzv@gmail.com>
2025-05-22 17:29:41 +0300

    deleted web server folder

--- web-server/backend/config.ts
+++
@@ -1,6 +0,0 @@
-export const MQTT_BROKER_URL = "mqtt://localhost:1883";
-export const MQTT_TOPIC = "camera/images";
-export const IMAGE_SAVE_PATH = "./images/";
-export const mongoUrl = "mongodb://localhost:27017";
-export const dbName = "imageDB";
-export const port = 3000;
